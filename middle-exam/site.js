b1fcb3c5c252bc08ecb815b1f96efacf8b0aeea5
KingFarGrace <KingFarGrace.163.com>
2020-12-05 19:05:33 +0800

    Some Change + Adjust main page's layout. + Add animation to main page. + Build sub pages.

--- middle-exam/site.js
+++ middle-exam/site.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    
+    $("#hero-profile").hover(
+        function(){
+            $("h4.card-title").animate({fontSize: '40px'});
+            $("h6.card-subtitle").animate({fontSize: '24px'});
+        }, 
+        function(){
+            $("h4.card-title").animate({fontSize: '24px'});
+            $("h6.card-subtitle").animate({fontSize: '16px'});
+        }
+    );
 });
